a0d6ce34416ed4301c35a139f2809ef61ab73d60
james <user@ES302N1>
2018-10-02 11:59:48 +0800

    third

--- com.fju/src/com/fju/Hello.java
+++ com.fju/src/com/fju/Hello.java
@@ -7,12 +7,14 @@ public class Hello {
 			System.out.println("Hello world");
 	int age = 19;
 	float weight = 86.4f;
-	float height = 179.9f;
+	float height = 1.8f;
 	String name = "james tseng" ;
 	System.out.println("name:" + name );
 	System.out.println("age:" + age );
 	System.out.println("weight:" + weight );
 	System.out.println("height:" + height );
+	float bmi = weight / (height * height );
+	System.out.println("bmi:" + bmi );
 		}
 	
 
